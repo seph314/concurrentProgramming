80e522aafb7f843551d0ef3d41ff2d4c029bba81
Anders <andpet52@kth.se>
2018-01-19 10:40:16 +0100

    initial commit

---
+++ src/Driver.java
@@ -0,0 +1,28 @@
+/**
+ * Driver class that uses QuickSort Threads
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        startSomeThreads();
+    }
+
+    /**
+     * Starts some threads
+     */
+    public static void startSomeThreads(){
+        QuickSort quickSort;
+
+        // start 10 threads
+        for (int i=10; i > 0; i--){
+            System.out.println("Thread: " + new QuickSort().getName() + " up and running");
+        }
+    }
+
+    /**
+     * Shows number of cores on your comuter
+     */
+    public void showNumberOfCores() {
+        System.out.println("Number of cores: " + Runtime.getRuntime().availableProcessors());
+    }
+}
---
+++ src/QuickSort.java
@@ -0,0 +1,10 @@
+/**
+ * Quicksort extends Thread and implements it'own version of the run() function
+ */
+public class QuickSort extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
