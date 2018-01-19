1b049a777c7a9c74ea02e0ab104650faf857952f
Anders <andpet52@kth.se>
2018-01-19 12:45:37 +0100

    QuickSort implemented

--- src/Driver.java
+++ src/Driver.java
@@ -5,17 +5,41 @@ public class Driver {
 
     public static void main(String[] args) {
         startSomeThreads();
+
+        /*
+        * QuickSort test
+        * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+        int[] array = {3,1,4,1,5,9,2,6,5,3,5};
+
+        // prints before
+        for (int i :array){
+            System.out.println("Unsorted array: " + i);
+        }
+
+        // sorts
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(array);
+
+        // prints after
+        for (int i :array){
+            System.out.println("QuickSorted array: " + i);
+        }
+        /*
+         * END QuickSort test
+         * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     }
 
     /**
      * Starts some threads
      */
     public static void startSomeThreads(){
-        QuickSort quickSort;
+
 
         // start 10 threads and prints their names
         for (int i=10; i > 0; i--){
-            System.out.println("Thread: " + new QuickSort().getName() + " up and running");
+            QuickSort qs = new QuickSort();
+            qs.start();
+            System.out.println("Thread: " + qs.getName() + " up and running");
         }
     }
 
@@ -25,4 +49,5 @@ public class Driver {
     public void showNumberOfCores() {
         System.out.println("Number of cores: " + Runtime.getRuntime().availableProcessors());
     }
+
 }
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -3,8 +3,60 @@
  */
 public class QuickSort extends Thread {
 
+
     @Override
     public void run() {
-        super.run();
+
+    }
+
+    /*
+    * Creates left and right value and calls the correct method with these
+    * */
+    void quickSort(int[] array){
+        quickSort(array, 0, array.length-1);
+
+    }
+
+    /**
+     * This shoule be implemented in the run() function I think...
+     * @param array is the array that we want to sort
+     * @param left is the leftmost value in the array
+     * @param right is the rightmost value in teh array
+     */
+    void quickSort(int array[], int left, int right) {
+
+        int index = partition(array, left, right);
+
+        if (left < index - 1)
+            quickSort(array, left, index - 1);
+
+        if (index < right)
+            quickSort(array, index, right);
+    }
+
+    private int partition(int array[], int left, int right)
+
+    {
+        int i = left, j = right;
+        int tmp;
+        int pivot = array[(left + right) / 2];
+
+        while (i <= j) {
+            while (array[i] < pivot)
+                i++;
+
+            while (array[j] > pivot)
+                j--;
+
+            if (i <= j) {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        return i;
     }
 }
